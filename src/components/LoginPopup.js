af9cfb03af4657072f22c820672f7573c2ec7544
dkuku <daniel.kuku@gmail.com>
2018-04-04 21:05:22 +0100

    login works with data from formular

--- src/components/LoginPopup.js
+++ src/components/LoginPopup.js
@@ -12,32 +12,46 @@ const inlineStyle = {
     marginRight: 'auto',
   },
 }
+const headers = {
+  Accept: 'application/json, text/plain, */*',
+  'Content-Type': 'application/json',
+}
+function body(email, password) {
+  return JSON.stringify({
+    sign_in: {
+      email: email,
+      password: password,
+    },
+  })
+}
+
 class LoginPopup extends Component {
-  state = { modalOpen: false }
-  handleOpen = () =>
+  state = {
+    modalOpen: false,
+    first_name: '',
+    last_name: '',
+    password: '',
+    password_confirmation: '',
+    email: '',
+  }
+  handleChange = (e, { name, value }) => this.setState({ [name]: value })
+
+  handleSubmit = () =>
     fetch('api/sign_in', {
       method: 'post',
-      headers: {
-        Accept: 'application/json, text/plain, */*',
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        sign_in: {
-          email: 'dan@wp.pl',
-          password: '123qwe',
-        },
-      }),
+      headers: headers,
+      body: body(this.state.email, this.state.password),
     })
       .then(res => res.json())
       .then(data => {
-        console.log(data.data.user)
         this.props.login({ user: data.data.user })
       })
 
-  //handleOpen = () => this.setState({ modalOpen: true })
+  handleOpen = () => this.setState({ modalOpen: true })
 
   handleClose = () => this.setState({ modalOpen: false })
   render() {
+    const { first_name, last_name, email, password, password_confirmation } = this.state
     return (
       <Modal
         trigger={<Button onClick={this.handleOpen}>Show Modal</Button>}
@@ -48,9 +62,29 @@ class LoginPopup extends Component {
         <Modal.Header>Login</Modal.Header>
         <Modal.Content>
           <Modal.Description>
-            <Form>
-              <Form.Input label="First name" placeholder="First name" />
-              <Form.Input label="Last name" placeholder="Last name" />
+            <Form onSubmit={this.handleSubmit}>
+              <Form.Input
+                label="First name"
+                placeholder="First name"
+                name="first_name"
+                value={first_name}
+                onChange={this.handleChange}
+              />
+              <Form.Input
+                label="Last name"
+                placeholder="Last name"
+                name="last_name"
+                value={last_name}
+                onChange={this.handleChange}
+              />
+              <Form.Input label="Email" placeholder="email" name="email" value={email} onChange={this.handleChange} />
+              <Form.Input label="Password" value={password} name="password" onChange={this.handleChange} />
+              <Form.Input
+                label="Confirm Password"
+                name="password_confirmation"
+                value={password_confirmation}
+                onChange={this.handleChange}
+              />
               <Form.Checkbox label="I agree to the Terms and Conditions" />
               <Button type="submit">Submit</Button>
             </Form>
