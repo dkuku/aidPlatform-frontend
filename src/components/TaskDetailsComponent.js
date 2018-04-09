be2ff189658004a2cd64cabadc0d8b47af9716db
dkuku <daniel.kuku@gmail.com>
2018-04-09 23:44:02 +0100

    Task details from generator

---
+++ src/components/TaskDetailsComponent.js
@@ -0,0 +1,23 @@
+'use strict'
+
+import React from 'react'
+
+require('styles/TaskDetails.css')
+
+class TaskDetailsComponent extends React.Component {
+  render() {
+    return (
+      <div className="taskdetails-component">
+        Please edit src/components//TaskDetailsComponent.js to update this component!
+      </div>
+    )
+  }
+}
+
+TaskDetailsComponent.displayName = 'TaskDetailsComponent'
+
+// Uncomment properties you need
+// TaskDetailsComponent.propTypes = {};
+// TaskDetailsComponent.defaultProps = {};
+
+export default TaskDetailsComponent
