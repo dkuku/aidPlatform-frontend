c7e355645dfd1fb240e46005adf5738c616656b0
Daniel Kukula <daniel.kuku@gmail.com>
2018-04-15 02:14:38 +0100

    removed some console logs

--- src/reducers/mapCoords.js
+++ src/reducers/mapCoords.js
@@ -3,13 +3,11 @@ import { UPDATE_BOUNDS, UPDATE_CENTER } from 'constants/ActionTypes'
 export default function mapCoords(state = {}, action) {
   switch (action.type) {
     case UPDATE_BOUNDS:
-      console.log(111, action)
       return {
         ...state,
         bounds: action.payload,
       }
     case UPDATE_CENTER:
-      console.log(2111, action)
       return {
         ...state,
         center: action.payload,
