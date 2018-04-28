95f0a24a27a911cdac6518293ff1ad3bba425d02
dkuku <daniel.kuku@gmail.com>
2018-04-28 02:27:57 +0100

    messages menu

--- src/containers/VolunteerContainer.js
+++ src/containers/VolunteerContainer.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
-import { Grid } from 'semantic-ui-react'
+import { Grid, Menu } from 'semantic-ui-react'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 import axios from 'axios'
@@ -16,30 +16,48 @@ class VolunteerContainer extends Component {
   constructor(props) {
     super(props)
     this.state = {
+      activeItem: '',
       id: Number(this.props.match.params.id),
       marker: this.props.markers.filter(obj => {
         obj.id == this.props.match.params.id
       })[0],
     }
   }
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+
+  componentWillMount() {
+    console.log(this.props.markers.length)
+    if (this.props.markers.length === 1) {
+      this.props.getMarkers()
+    }
+  }
+
   componentDidMount() {
-    console.log(this.state)
     const headers = { headers: { 'AUTH-TOKEN': this.props.user.authentication_token } }
-    console.log(headers)
     this.props.getConversations(this.state.id, headers)
   }
   render() {
-    const marker = this.props.markers.filter(obj => {
-      obj.id == this.state.id
-    })[0]
+    const { activeItem } = this.state
+    var name
     this.props.updateActiveIndex(this.state.id)
     return (
       <Grid divided="vertically">
         <Grid.Row columns={2}>
-          <Grid.Column>{/*
-           <MarkerDisplay marker={this.marker} />
-          */}</Grid.Column>
           <Grid.Column>
+            {!(this.props.markers.length === 1) && (
+              <MarkerDisplay marker={this.props.markers.filter(obj => obj.id == this.state.id)[0]} />
+            )}
+          </Grid.Column>
+          <Grid.Column>
+            <Menu attached="top" tabular>
+              {this.props.conversationsReducers.map(conversation => (
+                <Menu.Item
+                  name={conversation.first_name + ' ' + conversation.last_name}
+                  active={activeItem === conversation.first_name + ' ' + conversation.last_name}
+                  onClick={this.handleItemClick}
+                />
+              ))}
+            </Menu>
             {this.props.conversationsReducers.map(conversation => <MessagesContainer conversation={conversation} />)}
           </Grid.Column>
         </Grid.Row>
