a1b166fcaceb8dd2af545b44658912de4ba6063b
Daniel Kukula <daniel.kuku@gmail.com>
2018-05-07 15:48:43 +0100

    Different componet for displaying task on user/valunteer page

---
+++ src/components/TaskDetails.js
@@ -0,0 +1,33 @@
+import React, { PureComponent } from 'react'
+import { Item, Image, Icon, Button } from 'semantic-ui-react'
+import { Link } from 'react-router-dom'
+
+class TaskDetails extends PureComponent {
+  render() {
+    const { title, id, task_type, description, done, fulfilment_counter, user_id } = this.props.marker
+    const color = task_type === 'material' ? 'blue' : 'green'
+
+    return (
+      <Item.Group>
+        <Item>
+          <Item.Image size="medium" src="/images/bbq.jpeg" />
+          <Item.Content>
+            <Item.Header>{title}</Item.Header>
+            <Item.Meta>
+              <span>status: {done ? 'done' : 'waiting'}</span>
+              <span>type: {task_type}</span>
+            </Item.Meta>
+            <Item.Description>{description}</Item.Description>
+          </Item.Content>
+          <Item.Extra>
+            <Button basic color={color}>
+              Mark Fulfiled
+            </Button>
+          </Item.Extra>
+        </Item>
+      </Item.Group>
+    )
+  }
+}
+
+export default TaskDetails
